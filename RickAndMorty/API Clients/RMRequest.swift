0468f0a2863900ee88b5fe1f66ee17c6601a8fad
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-12-26 14:40:35 +0100

    Create API request

--- RickAndMorty/API Clients/RMRequest.swift
+++ RickAndMorty/API Clients/RMRequest.swift
@@ -9,5 +9,65 @@ import Foundation
 
 /// Object that  represent a single api call
 class RMRequest {
+    /// API Constant
+    private struct Constants {
+        static let baseUrl = "https://rickandmortyapi.com/api"
+    }
     
+    /// Desired Endpoint
+    private let endpoint: RMEndpoint
+    
+    /// Path component for APi, if  any
+    private let pathComponents: Set<String>
+    
+    /// Query Argument for APi, if  any
+    private let queryParameters: [URLQueryItem]
+    
+    /// Constructed url for the api request in string format
+    private var urlString: String  {
+        var string = Constants.baseUrl
+        string += "/"
+        string += endpoint.rawValue
+        
+        if !pathComponents.isEmpty {
+            pathComponents.forEach { string += "/\($0)"}
+        }
+        
+        if !queryParameters.isEmpty {
+            string += "?"
+            let argumentString = queryParameters.compactMap({
+                guard let value = $0.value else {  return nil }
+                return  "\($0.name)=\(value)"
+            }).joined(separator: "&")
+            
+            string += argumentString
+        }
+        
+        return string
+    }
+    
+    ///Computed & constructed API url
+    public var url: URL? {
+        return URL(string: urlString)
+    }
+    
+    /// Desired HTTP Method
+    public let httpMethod = "GET"
+    
+    // MARK: - Public
+    
+    /// Constructed request
+    /// - Parameters:
+    ///   - endpoint: Target endpoint
+    ///   - pathComponents: Collection of path component
+    ///   - queryParameters: Collection of query parameters
+    init(
+        endpoint: RMEndpoint,
+        pathComponents: Set<String> = [],
+        queryParameters: [URLQueryItem] = []
+    ) {
+        self.endpoint = endpoint
+        self.pathComponents = pathComponents
+        self.queryParameters = queryParameters
+    }
 }
--- RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 /// Controller to show and search characters
 final class RMCharacterViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Characters"
+        
+        let request = RMRequest(
+            endpoint: .character,
+            queryParameters: [
+                URLQueryItem(name: "name", value: "rick"),
+                URLQueryItem(name: "status", value: "alive")
+            ])
+        
+        RMService.shared.execute(request, expecting: RMCharacter.self) { result in
+            switch result {
+            case let .success(character): break
+            case let .failure(error):
+                break
+            }
+        }
     }
-
+    
 }
